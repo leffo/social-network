c5e0bac810a274331263a2e63e416e9d579a1a88
Alex Yakovlev <leffalt@gmail.com>
2024-01-01 16:12:51 +0700

    screwed up tailwind

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="text-3xl font-bold underline">
+        <h1 class="text-5xl font-bold underline">
             Hello world!
         </h1>
         <router-link to="index">Index</router-link>
